073c419f900cf41157face808abc531bffbf9d47
EcheverriaClau <echeverria.cla@gmail.com>
2025-07-06 23:18:42 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,16 @@
+const CACHE_NAME = 'estrategias-ayla-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/visual.css',
+  '/estrategiasBase.js',
+  '/estructurasBase.js',
+  '/visual-pro.js',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/manifest.json'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('estrategias-cache').then((cache) => {
